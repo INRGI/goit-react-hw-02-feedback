dbc625c677f44bf00ee94cadef4050b254e53ecb
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-11-29 19:02:06 +0100

    add notification component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import Notification from "components/Notification";
+import { Component } from "react";
+
+export class App extends Component{
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <p>React homework template</p>
+        <Notification message="There is no feedback"/>
+      </div>
+    );
+  }
 };
