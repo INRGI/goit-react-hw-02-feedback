c092073c110ed7aa2359283dd7ae8a379caabc59
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-12-01 07:55:20 +0100

    add statstcs component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import Notification from "components/Notification";
 import Section from "components/Section";
+import Statictics from "components/Statictics";
 import { Component } from "react";
 
 export class App extends Component{
@@ -9,12 +10,36 @@ export class App extends Component{
     bad: 0
   };
 
+  totalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  positivePercentage = () => {
+    const { good } = this.state;
+    const total = this.totalFeedback();
+    return Math.round((good / total) * 100) || 0;
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.totalFeedback();
+    const percentage = this.positivePercentage();
+
     return (
       <div>
         <Section title="Please leave feedback"></Section>
         <Section title="Statictics">
-          <Notification message="There is no feedback"/>
+          {total !== 0 ? (
+            <Statictics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={percentage} />
+          ) : (
+            <Notification message="There is no feedback" />
+          )};
         </Section>
       </div>
     );
